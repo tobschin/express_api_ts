e40bbc40a6f9697d32814cbf12e480f02a03612d
tobiasschinagl <tobias.schinagl@codecentric.de>
2022-10-10 13:20:05 +0200

    add cat

---
+++ src/cat.ts
@@ -0,0 +1,4 @@
+export default interface Cat {
+    name : String;
+    age : Number;
+}
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,15 @@
-import express, { Request, Response, Router } from 'express';
-let router : Router = express.Router()
+import express, {Request, Response, Router} from 'express';
+import Cat from './cat'
 
-router.get('/', (req : Request, res : Response)=> {
-    res.json({message: 'im on fire'})
+let router: Router = express.Router()
+
+router.get('/', (req: Request, res: Response) => {
+  res.json({message: 'im on fire'})
+})
+
+router.post('/', (req: Request, res: Response) => {
+  let cat: Cat = {name: req.body.name, age: req.body.age}
+  res.json(cat)
 })
 
 export default router;
